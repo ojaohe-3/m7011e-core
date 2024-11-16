fd4bb68fcdea2d456eeff901cdae9309b38ccdb7
Johan Rodahl Holmgren <itgsod.johan.holmgren@gmail.com>
2024-11-16 20:18:35 +0100

    Removed Kotlin

--- src/main/java/m7011e/the_homeric_odyssey/core/CoreApplication.java
+++ src/main/java/m7011e/the_homeric_odyssey/core/CoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreVendorAppApplication {
+public class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreVendorAppApplication.class, args);
+		SpringApplication.run(CoreApplication.class, args);
 	}
 
 }
